25ea5ab5e94e23f97354d6a867337f44a61813a8
Franco Torres <104739161+Kaitach@users.noreply.github.com>
2023-01-19 19:45:34 -0300

    Agregando un espacio

--- Problema9/src/index.ts
+++ Problema9/src/index.ts
@@ -1,24 +1,18 @@
 //Creamos la funcion
 function imprimirArbol(n: number) {
-
-
   for (let i = 0; i < n; i++) {
   let espacio:string = " " ;
   let estrellas:string = " " ;
-
   for (let j = 0; j < n - i - 1; j++) {
     espacio += " ";
 }
-
 for (let j = 0; j < i * 2 + 1; j++) {
   estrellas += "*";
 }
-
-
 console.log(espacio + estrellas)
 
  }
- let base:string =" " ;
+ let base:string ="  " ;
 
  for (let i = 0; i < n - 2 ; i++) {
    base += " ";
