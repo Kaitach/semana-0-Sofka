4fa34c4dfbe6b17b835639566671be03190e001e
Franco Torres <104739161+Kaitach@users.noreply.github.com>
2023-01-19 17:58:04 -0300

    Sacando console.log

--- Problema1/src/index.ts
+++ Problema1/src/index.ts
@@ -1,8 +1,8 @@
 const PI = 3.14159
 
-function areaCirculo(radio: number){
+function areaCirculo(radio: number):number{
     let area = PI * (radio**2);
-    console.log(area);
+    return area;
 }
 
-areaCirculo(10)
+console.log(`Primer area ${areaCirculo(10)} \nSegunda area ${areaCirculo(15)} \nTercer area ${areaCirculo(2)} `)
